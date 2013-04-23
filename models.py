528a8939394407ecfd597643c134a87e3b7dc084
Akshar Raaj <akshar@agiliq.com>
2013-04-23 22:59:52 +0530

    Added model for User

--- models.py
+++ models.py
@@ -17,3 +17,18 @@ class Entry(Base):
 
     def __repr__(self):
         return self.title
+
+class User(Base):
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return self.username
