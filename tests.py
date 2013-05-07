74bc48f61b007146385f07611df09ffecb32f94c
Akshar Raaj <akshar@agiliq.com>
2013-05-07 11:46:12 +0530

    Do not drop the tables after each test

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ from flask.ext.testing import TestCase
 import main #To load the urls and views
 import flaskr
 from flaskr import app, db
-from models import User
+from models import User, Entry
 
 flaskr.app.config.from_object('test_settings')
 
@@ -15,28 +15,32 @@ class FlaskrTestCase(TestCase):
         return app
 
     def setUp(self):
-        db.create_all()
+        pass
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        User.query.delete()
+        Entry.query.delete()
+        db.session.commit()
 
     def test_empty_db(self):
-        resp = self.client.get('/')
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue("Unbelievable. No entries here so far" in resp.data)
+        with self.app.test_client() as client:
+            resp = client.get('/')
+            self.assertEqual(resp.status_code, 200)
+            self.assertTrue("Unbelievable. No entries here so far" in resp.data)
 
     def test_register(self):
-        resp = self.client.post("/register", data={'username': 'test', 'password': 'test'}, follow_redirects=True)
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue("You are registered" in resp.data)
-        self.assertEqual(User.query.filter_by(username='test').count(), 1)
+        with self.app.test_client() as client:
+            resp = client.post("/register", data={'username': 'test', 'password': 'test'}, follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertTrue("You are registered" in resp.data)
+            self.assertEqual(User.query.filter_by(username='test').count(), 1)
 
     def test_login(self):
-        self.client.post("/register", data={'username': 'test', 'password': 'test'}, follow_redirects=True)
-        resp = self.client.post("/login", data={'username': 'test', 'password': 'test'}, follow_redirects=True)
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue("You were logged in" in resp.data)
+        with self.app.test_client() as client:
+            client.post("/register", data={'username': 'test', 'password': 'test'}, follow_redirects=True)
+            resp = client.post("/login", data={'username': 'test', 'password': 'test'}, follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertTrue("You were logged in" in resp.data)
 
 if __name__=='__main__':
     unittest.main()
